f92b48865c43fea229a810af3c541a84ab68b3a7
David Gomesh <davidgomesh2016@gmail.com>
2023-04-06 18:44:27 +0100

    Fixed messaging

--- src/main/java/com/isep/acme/messaging/ProductConsumer.java
+++ src/main/java/com/isep/acme/messaging/ProductConsumer.java
@@ -1,21 +1,37 @@
 package com.isep.acme.messaging;
 
+import java.io.IOException;
+
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 import com.isep.acme.domain.model.Product;
 import com.isep.acme.domain.service.ProductService;
+import com.isep.acme.dto.mapper.ProductMapper;
+import com.isep.acme.dto.message.ProductMessage;
+import com.rabbitmq.client.Channel;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 @AllArgsConstructor
 public class ProductConsumer {
 
     private final ProductService productService;
+    private final ProductMapper productMapper;
     
     @RabbitListener(queues = {"#{productCreatedQueue.name}"})
-    public void productCreated(Product product){
+    public void productCreated(ProductMessage productMessage, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws IOException{
+
+        log.info("Product received: " + productMessage.getSku());
+        Product product = productMapper.toEntity(productMessage);
+        
         productService.create(product);
+        channel.basicAck(tag, false);
+        log.info("Product created: " + product.getSku());
     }
 }
--- src/main/java/com/isep/acme/messaging/ReviewConsumer.java
+++ src/main/java/com/isep/acme/messaging/ReviewConsumer.java
@@ -1,23 +1,52 @@
 package com.isep.acme.messaging;
 
+import java.io.IOException;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 import com.isep.acme.domain.model.Review;
 import com.isep.acme.domain.service.ReviewService;
-import com.isep.acme.dto.request.CreateReviewDTO;
+import com.isep.acme.dto.mapper.ReviewMapper;
+import com.isep.acme.dto.message.ReviewMessage;
+import com.rabbitmq.client.Channel;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 @AllArgsConstructor
 public class ReviewConsumer {
     
+    private final String instanceId;
+    private final MessageConverter messageConverter;
     private final ReviewService reviewService;
+    private final ReviewMapper reviewMapper;
 
-    @RabbitListener(queues = {"#{reviewCreatedQueue.name}"})
-    public void reviewCreated(Review review){
-        CreateReviewDTO createReviewDTO = new CreateReviewDTO(review.getReviewText(), review.getUser().getUserId(), review.getRating().getRate());
-        reviewService.create(createReviewDTO , review.getProduct().getSku());
+    @RabbitListener(queues = "#{reviewCreatedQueue.name}", ackMode = "MANUAL")
+    public void reviewCreated(Message message, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws IOException{
+        
+        MessageProperties messageProperties = message.getMessageProperties();
+        if(messageProperties.getAppId().equals(instanceId)){
+            channel.basicAck(tag, false);
+            log.info("Received own message and ignored it.");
+            return;
+        }
+
+        ReviewMessage reviewMessage = (ReviewMessage) messageConverter.fromMessage(message);
+        log.info("Review received: " + reviewMessage.getIdReview());
+
+        Review review = reviewMapper.toEntity(reviewMessage);
+        reviewService.save(review);
+        log.info("Review created: " + reviewMessage.getIdReview());
+
+        channel.basicAck(tag, false);
     }
+    
 }
