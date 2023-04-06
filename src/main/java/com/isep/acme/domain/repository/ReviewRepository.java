5f36bb2c7d29eec2609094b74d460e7f3c5ac817
David Gomesh <davidgomesh2016@gmail.com>
2023-04-06 18:17:48 +0100

    Improved review repository

--- src/main/java/com/isep/acme/domain/repository/ReviewRepository.java
+++ src/main/java/com/isep/acme/domain/repository/ReviewRepository.java
@@ -1,30 +1,20 @@
 package com.isep.acme.domain.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.isep.acme.domain.model.Product;
 import com.isep.acme.domain.model.Review;
-import com.isep.acme.domain.model.User;
+import com.isep.acme.domain.model.enumarate.ApprovalStatus;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
+    List<Review> findByProduct(Product product);
 
-    @Query("SELECT r FROM Review r WHERE r.product=:product ORDER BY r.publishingDate DESC")
-    Optional<List<Review>> findByProductId(Product product);
+    List<Review> findByApprovalStatus(ApprovalStatus approvalStatus);
 
-    @Query("SELECT r FROM Review r WHERE r.approvalStatus='pending'")
-    Optional<List<Review>> findPendingReviews();
+    List<Review> findByProductAndApprovalStatus(Product product, String status);
 
-    @Query("SELECT r FROM Review r WHERE r.approvalStatus='active'")
-    Optional<List<Review>> findActiveReviews();
-
-    @Query("SELECT r FROM Review r WHERE r.product=:product AND r.approvalStatus=:status ORDER BY r.publishingDate DESC")
-    Optional<List<Review>> findByProductIdStatus(Product product, String status);
-
-    @Query("SELECT r FROM Review r WHERE r.user=:user ORDER BY r.publishingDate DESC")
-    Optional<List<Review>> findByUserId(User user);
+    List<Review> findByUser(String user);
 }
